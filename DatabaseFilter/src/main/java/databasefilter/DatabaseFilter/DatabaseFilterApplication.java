5baf37755a742b8d6c3f1b815a805e35a4ac0018
muhameddaahmed <ma2217000@gmail.com>
2023-05-21 22:18:18 +0300

    Update DatabaseFilterApplication.java

--- DatabaseFilter/src/main/java/databasefilter/DatabaseFilter/DatabaseFilterApplication.java
+++ DatabaseFilter/src/main/java/databasefilter/DatabaseFilter/DatabaseFilterApplication.java
@@ -1,7 +1,12 @@
 package databasefilter.DatabaseFilter;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DatabaseFilterApplication {
@@ -10,4 +15,12 @@ public class DatabaseFilterApplication {
 		SpringApplication.run(DatabaseFilterApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration= new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList(("http://localhost:4200")));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept",
+				"Authorization", "Origin, Accept", "X-Requested-With", "Access-Control"));
+	}
 }
